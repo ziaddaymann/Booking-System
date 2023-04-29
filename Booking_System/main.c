6e414413b6e7728b4b3ff282bd8e681fbed604ca
Ziad Ayman <ziad991488@gmail.com>
2023-04-29 07:26:56 +0300

    Adding Round Trip function in Flight Mode

--- Booking_System/main.c
+++ Booking_System/main.c
@@ -13,6 +13,15 @@
 #include <ctype.h>
 #include <regex.h>
 
+///////////////////////////// ************** Defines ******************///////////////////////
+
+#define ONEWAY 1
+#define ROUNDTRIP 2
+#define MULTIY 3
+
+///////////////////////////// ************** Functions Prototype ******************///////////////////////
+
+
 void Main_Menu ();
 void Menu();
 void Register ();
@@ -22,6 +31,7 @@ void Website();
 void Oneway();
 void Flight();
 void Roundtrip();
+void Prices (char degree);
 void Start();
 
 
@@ -35,6 +45,7 @@ void Main_Menu ()
     printf ("\nIf you forgot your password enter y\n");
 }
 
+
 void Menu()
 {
 	printf ("\nBook Flight enter a\n");
@@ -46,6 +57,7 @@ void Menu()
     printf ("\nDelete your account enter h\n");
 }
 
+
 void Register ()
 {
 	FILE *fp;
@@ -54,7 +66,7 @@ void Register ()
 	char buffer[1024];
 	char file[256] = "C:/Users/imann/Desktop/Booking-System-Mybranch/Book1.txt";
 
-	///////////////////////////Mail///////////////////////
+//////////////////////////////Mail////////////////////////////////////////////////////
 	printf ("Please enter your Email \n");
 	scanf("%s", mail);
 	char *find1 = strchr(mail, '@');
@@ -91,7 +103,7 @@ void Register ()
 	        }
 	    }
 
-	///////////////////////////////////Pass///////////////////////
+//////////////////////////////Password////////////////////////////////////////////////////
 
 
 	printf ("Please enter you Password \n");
@@ -105,7 +117,8 @@ void Register ()
 
 
 
-	//////////////////////////////////////Adding///////////////////
+//////////////////////////////Adding////////////////////////////////////////////////////
+
 
 	fp = fopen(file, "a");
 
@@ -120,14 +133,17 @@ void Register ()
 	printf ("\nSign up successful\n");
 }
 
+
 void Log_in ()
 {
 	char mail[50];
 	char pass[50];
 	char buffer[1024];
-	char f;
+	char f=0;
 	char file[256] = "/Users/ziadayman/Desktop/Booking Site/Book1.txt";
 
+
+//////////////////////////////Mail////////////////////////////////////////////////////
 
 	printf ("Please enter your Email \n");
 	scanf("%s", mail);
@@ -149,6 +165,9 @@ void Log_in ()
 		Start();
 	}
 
+//////////////////////////////Password////////////////////////////////////////////////////
+
+
 	printf ("Please enter the password\n");
 	scanf("%s",pass);
 
@@ -159,6 +178,7 @@ void Log_in ()
 	}
 
 
+//////////////////////////////File////////////////////////////////////////////////////
 
 	FILE *fp = fopen(file, "r");
 	    if (fp == NULL)
@@ -193,6 +213,7 @@ void Log_in ()
 	    }
 }
 
+
 void forget_pass()
 {
 	FILE *fp;
@@ -200,8 +221,9 @@ void forget_pass()
 	char pass[50];
 	char buffer[1024];
 	char file[256] = "/Users/ziadayman/Desktop/Booking Site/Book1.txt";
-	char f;
+	char f=0;
 
+//////////////////////////////Mail////////////////////////////////////////////////////
 
 	printf ("Please enter your Email \n");
 	scanf("%s", mail);
@@ -222,6 +244,8 @@ void forget_pass()
 			printf ("Invalid Mail\n");
 			Register();
 		}
+
+//////////////////////////////File////////////////////////////////////////////////////
 
 		 fp = fopen(file, "r");
 		    if (fp == NULL)
@@ -245,6 +269,9 @@ void forget_pass()
 
 		    fclose(fp);
 
+//////////////////////////////Password////////////////////////////////////////////////////
+
+
 		    if (f)
 		    {
 		         printf("Your password is: %s\n", pass);
@@ -256,6 +283,7 @@ void forget_pass()
 		         Start();
 		    }
 }
+
 
 void Website()
 {
@@ -279,11 +307,21 @@ void Website()
 
 ///////////////////////////// ************** Flight Information ******************///////////////////////
 
+void Prices (char degree)
+{
+	switch (degree)
+	{
+	case ONEWAY: printf ("\n  Economy :\n Adults: 1000\n Childs: 500\n\n Premium Economy:\n Adults: 2000\n Childs: 1000\n\n Business :\n Adults: 5000\n Childs: 3000\n\n First class :\n Adults: 10000\n Childs: 6500\n\n ");
+	break;
+	}
+}
+
+
 void Oneway()
 {
 	FILE *fp , *tmp;
-	char adults [1],childs[1];
-	char check;
+	char adults [1],childs[1],degree[1];
+	char check,check1,ch;
 	int money;
 	char input[1];
 	char From[50];
@@ -294,6 +332,9 @@ void Oneway()
 	int f=0;
 	char file[256] = "/Users/ziadayman/Desktop/Booking Site/Dates.txt";
 	char file1[256] = "/Users/ziadayman/Desktop/Booking Site/Capitals.txt";
+
+//////////////////////////////Data////////////////////////////////////////////////////
+
 
 	printf ("\nFrom?\n");
 	printf ("\nPlease enter full capital Name with first uppercase letter\n");
@@ -372,26 +413,38 @@ void Oneway()
 	printf ("\nHour ? xx\n");
 	scanf("%d",&hours);
 
-	 tmp = fopen(file, "r");
-	    if (tmp == NULL)
-	    {
-	        printf("Error opening file %s\n", file);
-	    }
+//////////////////////////////File////////////////////////////////////////////////////
+	tmp = fopen(file, "r");
+		    if (tmp == NULL)
+		    {
+		        printf("Error opening file %s\n", file);
+		    }
 
-	    char line[256];
-	    int days, months, years, hour;
-	    int n_matches;
-	    char* pattern = "%d;%d;%d;%d";
+		    char line[256];
+		    int days, months, years, hour;
+		    int n_matches;
+		    char* pattern = "%d;%d;%d;%d";
 
-	    while (fgets(line, sizeof(line), tmp)) {
-	    n_matches = sscanf(line, pattern, &days, &months, &years, &hour);
-	    if (n_matches == 4 && days == day && months == month && years == year && hour == hours) {
-	    printf("Found date and time: %02d-%02d-%04d %02d:00\n", days, months, years, hour);
-	    f = 1;
-	    break;
-	    }
-	    }
-	    fclose(tmp);
+		    while (fgets(line, sizeof(line), tmp)) {
+		    n_matches = sscanf(line, pattern, &days, &months, &years, &hour);
+		    if (n_matches == 4 && days == day && months == month && years == year && hour == hours) {
+		    printf("Found date and time: %02d-%02d-%04d %02d:00\n", days, months, years, hour);
+		    f = 1;
+		    break;
+		    }
+		    }
+		    fclose(tmp);
+
+
+		    if (f)
+		    {
+		    	printf("\nThis flight is booked , Try Another Date\n");
+		    	Oneway();
+		    	f=0;
+		    }
+
+
+//////////////////////////////Money////////////////////////////////////////////////////
 
 	    printf ("How many Adults ?\n");
 	    scanf ("%s",adults);
@@ -409,7 +462,30 @@ void Oneway()
 		    scanf ("%s",childs);
 		}
 
-	    money = (check * 1000) + (atoi(childs) *500);
+		check1 = atoi(childs);
+
+		Prices(ONEWAY);
+
+		printf ("Enter your degree\n (Economy: 1 , Premium Economy: 2 , Business: 3 ,  First class: 4)\n");
+
+		scanf ("%s",degree);
+		ch = atoi(degree);
+
+		switch (ch)
+		{
+		case 1:  money = (check * 1000) + (check1 *500);
+		break;
+
+		case 2:  money = (check * 2000) + (check1 *1000);
+		break;
+
+		case 3:  money = (check * 5000) + (check1 *3000);
+		break;
+
+		case 4:  money = (check * 10000) + (check1 *6500);
+		break;
+		}
+
 
         printf ("\nThe total money from %s to %s at %02d-%02d-%04d %02d:00 is %d \n" , From , To , day , month , year , hours , money);
 
@@ -418,16 +494,6 @@ void Oneway()
 
         scanf ("%s",input);
 
-
-	    if (f)
-	    {
-	    	printf("\nThis flight is booked , Try Another Date\n");
-	    	Oneway();
-	    	f=0;
-	    }
-
-	    else
-	    {
 	    	if ((strcmp(input, "y") == 0) || (strcmp(input, "Y") == 0))
 	    	{
 	    		printf ("\nBooked\n ");
@@ -436,7 +502,8 @@ void Oneway()
 	    	{
 	    		Website();
 	    	}
-	    }
+
+ //////////////////////////////Adding////////////////////////////////////////////////////
 
 	    tmp = fopen(file, "a");
 
@@ -444,7 +511,7 @@ void Oneway()
 	    		{
 	    		        printf("Error opening file %s\n", file);
 	    		}
-	    		fprintf(fp, "%d;%d;%d;%d\n", day, month , year , hours);
+	    		fprintf(fp, "\n%02d;%02d;%04d;%02d", day, month, year, hours);
 
 	    	fclose(tmp);
 
@@ -455,10 +522,280 @@ void Oneway()
 
 }
 
+
 void Roundtrip()
 {
+	FILE *fp , *tmp;
+	char adults [1],childs[1],degree[1],degree1[1];
+	char check,check1,ch,ch1;
+	int money;
+	int money1;
+	int total;
+	char input[1];
+	char From[50];
+	char To[50];
+	int day,month,hours;
+	int year;
+	int day1,month1,hours1;
+	int year1;
+	char buffer[1024];
+	int f=0;
+	char file[256] = "/Users/ziadayman/Desktop/Booking Site/Dates.txt";
+	char file1[256] = "/Users/ziadayman/Desktop/Booking Site/Capitals.txt";
+
+//////////////////////////////Data////////////////////////////////////////////////////
+
+
+	printf ("\nFrom?\n");
+	printf ("\nPlease enter full capital Name with first uppercase letter\n");
+
+	scanf("%s",From);
+
+	 fp = fopen(file1, "r");
+	    if (fp == NULL)
+	    {
+	        printf("Error opening file %s\n", file1);
+	    }
+
+	    while (fgets(buffer, sizeof(buffer), fp) != NULL)
+	    {
+	        if (strstr(buffer, From) != NULL)
+	        {
+	            f=1;
+	            fclose(fp);
+	            break;
+	        }
+	    }
+	    fclose(fp);
+
+	    if (f)
+	    {
+	    	printf ("Founded\n");
+	    	f=0;
+	    }
+	    else
+	    {
+	    	printf ("Invalid Capital\n");
+	    	Flight();
+	    }
+
+	printf ("\nTo ?\n");
+
+	printf ("\nPlease enter full capital Name with first uppercase letter\n");
+
+	scanf("%s",To);
+
+	 fp = fopen(file1, "r");
+	    if (fp == NULL)
+	    {
+	        printf("Error opening file %s\n", file1);
+	    }
+
+	    while (fgets(buffer, sizeof(buffer), fp) != NULL)
+	    {
+	        if (strstr(buffer, To) != NULL)
+	        {
+	            f=1;
+	            fclose(fp);
+	            break;
+	        }
+	    }
+	    fclose(fp);
+
+	    if (f)
+	    {
+	    	printf ("Founded\n");
+	    	f=0;
+	    }
+	    else
+	    {
+	    	printf ("Invalid Capital\n");
+	    	Flight();
+	    }
+
+
+			printf ("\nGoing Day ? xx\n");
+			scanf("%d",&day);
+			printf ("\nGoing Month ? xx\n");
+			scanf("%d",&month);
+			printf ("\nGoing Year ? xxxx\n");
+			scanf("%d",&year);
+			printf ("\nGoing Hour ? xx\n");
+			scanf("%d",&hours);
+
+		//////////////////////////////File////////////////////////////////////////////////////
+			tmp = fopen(file, "r");
+				    if (tmp == NULL)
+				    {
+				        printf("Error opening file %s\n", file);
+				    }
+
+				    char line[256];
+				    int days, months, years, hour;
+				    int n_matches;
+				    char* pattern = "%d;%d;%d;%d";
+
+				    while (fgets(line, sizeof(line), tmp)) {
+				    n_matches = sscanf(line, pattern, &days, &months, &years, &hour);
+				    if (n_matches == 4 && days == day && months == month && years == year && hour == hours) {
+				    printf("Found date and time: %02d-%02d-%04d %02d:00\n", days, months, years, hour);
+				    f = 1;
+				    break;
+				    }
+				    }
+				    fclose(tmp);
+
+
+				    if (f)
+				    {
+				    	printf("\nThis flight is booked , Try Another Date\n");
+				    	Roundtrip();
+				    	f=0;
+				    }
+
+
+					printf ("\nComing Back Day ? xx\n");
+					scanf("%d",&day1);
+					printf ("\nComing Back Month ? xx\n");
+					scanf("%d",&month1);
+					printf ("\nComing Back Year ? xxxx\n");
+					scanf("%d",&year1);
+					printf ("\nComing Back Hour ? xx\n");
+					scanf("%d",&hours1);
+
+				//////////////////////////////File////////////////////////////////////////////////////
+					tmp = fopen(file, "r");
+						    if (tmp == NULL)
+						    {
+						        printf("Error opening file %s\n", file);
+						    }
+
+						    char line1[256];
+						    int days1, months1, years1, hour1;
+						    int n_matches1;
+						    char* pattern1 = "%d;%d;%d;%d";
+
+						    while (fgets(line1, sizeof(line1), tmp)) {
+						    n_matches1 = sscanf(line1, pattern1, &days1, &months1, &years1, &hour1);
+						    if (n_matches1 == 4 && days1 == day1 && months1 == month1 && years1 == year1 && hour1 == hours1) {
+						    printf("Found date and time: %02d-%02d-%04d %02d:00\n", days1, months1, years1, hour1);
+						    f = 1;
+						    break;
+						    }
+						    }
+						    fclose(tmp);
+
+
+						    if (f)
+						    {
+						    	printf("\nThis flight is booked , Try Another Date\n");
+						    	Roundtrip();
+						    	f=0;
+						    }
+
+
+//////////////////////////////Money////////////////////////////////////////////////////
+
+	    printf ("How many Adults ?\n");
+	    scanf ("%s",adults);
+
+	    check = atoi(adults);
+		if (check == 0)
+		{
+			printf ("Error\n");
+			Roundtrip();
+		}
+
+		else
+		{
+		    printf ("How many Childs ?\n");
+		    scanf ("%s",childs);
+		}
+
+		check1 = atoi(childs);
+
+		Prices(ONEWAY);
+
+		printf ("Enter your Going degree\n (Economy: 1 , Premium Economy: 2 , Business: 3 ,  First class: 4)\n");
+
+		scanf ("%s",degree);
+		ch = atoi(degree);
+
+		switch (ch)
+		{
+		case 1:  money = (check * 1000) + (check1 *500);
+		break;
+
+		case 2:  money = (check * 2000) + (check1 *1000);
+		break;
+
+		case 3:  money = (check * 5000) + (check1 *3000);
+		break;
+
+		case 4:  money = (check * 10000) + (check1 *6500);
+		break;
+		}
+
+		printf ("Enter your Coming Back degree\n (Economy: 1 , Premium Economy: 2 , Business: 3 ,  First class: 4)\n");
+
+		scanf ("%s",degree1);
+		ch1 = atoi(degree1);
+
+		switch (ch1)
+		{
+		case 1:  money1 = (check * 1000) + (check1 *500);
+		break;
+
+		case 2:  money1 = (check * 2000) + (check1 *1000);
+		break;
+
+		case 3:  money1 = (check * 5000) + (check1 *3000);
+		break;
+
+		case 4:  money1 = (check * 10000) + (check1 *6500);
+		break;
+		}
+
+		total = money+money1;
+
+
+        printf ("\nThe Going trip money from %s to %s at %02d-%02d-%04d %02d:00 is %d \n" , From , To , day , month , year , hours , money);
+        printf ("\nThe Coming Back trip money from %s to %s at %02d-%02d-%04d %02d:00 is %d \n" , To , From , day1 , month1 , year1 , hours1 , money1);
+
+        printf ("\nThe total money is %d\n" ,total);
+
+	    printf ("\nDo you want to continue Y/N\n");
+
+        scanf ("%s",input);
+
+	    	if ((strcmp(input, "y") == 0) || (strcmp(input, "Y") == 0))
+	    	{
+	    		printf ("\nBooked\n ");
+	    	}
+	    	else
+	    	{
+	    		Website();
+	    	}
+
+ //////////////////////////////Adding////////////////////////////////////////////////////
+
+	    tmp = fopen(file, "a");
+
+	    		if (fp == NULL)
+	    		{
+	    		        printf("Error opening file %s\n", file);
+	    		}
+	    		fprintf(fp, "\n%02d;%02d;%04d;%02d", day, month, year, hours);
+	    		fprintf(fp, "\n%02d;%02d;%04d;%02d", day1, month1, year1, hours1);
+
+	    	fclose(tmp);
+
+	    	Website();
+
+
 
 }
+
 
 void Flight()
 {
@@ -479,7 +816,8 @@ void Flight()
     case 'A':Oneway();
     break;
 
-    case 2: Roundtrip();
+    case 'b':
+    case 'B':Roundtrip();
     break;
 /*
     case 3: Multiple();
@@ -489,9 +827,6 @@ void Flight()
     }
 }
 }
-
-
-
 
 ///////////////////////////// ************** Start ******************////////////////////////////////////
 
@@ -515,12 +850,12 @@ void Start()
 
 		case 'b':
 		case 'B': Log_in();
-		Website();
+		          Website();
 		break;
 
 		case 'y':
 		case 'Y': forget_pass();
-		Start();
+		          Start();
 		break;
 
 		default:
